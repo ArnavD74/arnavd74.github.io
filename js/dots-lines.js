0d9179a4e16207f1e47b268e5705a5eeb35cbf0b
Wingo206 <50254747+Wingo206@users.noreply.github.com>
2019-05-04 00:21:44 -0400

    Big Succ on mouse click
    clicking will succ and also some bug fixes such as highlighting the header text

--- js/dots-lines.js
+++ js/dots-lines.js
@@ -1,11 +1,12 @@
+var home = document.getElementById("home");
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 var cw = 0;
 var ch = 0;
 var dots = [];
-var dotCount = 100;
+var dotCount = 200;
 var connectionDistance = 100;
-var maxSize = 5;
+var maxSize = 10;
 var minSize = 2;
 var minTransparency = 0.5;
 var maxTransparency = 1;
@@ -17,6 +18,9 @@ var my = -1000;
 var repelFactor = 10000000;
 var maxAccel = 5;
 var friction = 0.90;
+var pullFactor = 1000000;
+var pullDistance = 100;
+var mouseDown = false;
 setup();
 function setup () {
 	//set the starting size
@@ -24,16 +28,31 @@ function setup () {
 	for (var i = 0; i < dotCount; i++) {
 		dots.push(new dot());
 	}
-	c.addEventListener("mousemove", function(e) {
-		mx = e.clientX;
-		my = e.clientY;
-	});
+	home.addEventListener("mousemove", onMouseMove, true);
+	c.addEventListener("mousemove", onMouseMove, true);
+	home.addEventListener("mousedown", onMouseDown, true);
+	c.addEventListener("mousedown", onMouseDown, true);
+	home.addEventListener("mouseup", onMouseUp, true);
+	c.addEventListener("mouseup", onMouseUp, true);
+}
+
+function onMouseMove (e) {
+	mx = e.clientX;
+	my = e.clientY+document.documentElement.scrollTop;
+} 
+function onMouseDown (e) {
+	console.log("a");
+	mouseDown = true;
+}
+function onMouseUp (e) {
+	console.log("a");
+	mouseDown = false;
 }
 
 function onresize () {
-	cw = document.getElementById("home").clientWidth;
+	cw = home.clientWidth;
 	canvas.width = cw;
-	ch = document.getElementById("home").clientHeight;
+	ch = home.clientHeight;
 	canvas.height = ch;
 	update();
 }
@@ -59,21 +78,46 @@ function dot () {
 	this.xvel = maxSpeed * (Math.random() * 2  - 1);
 	this.yvel = maxSpeed * (Math.random() * 2 - 1);
 	this.t = minTransparency + Math.random()*maxTransparency;
+	this.applyForce = function (angle, force) {
+		this.x += Math.cos(angle) * force;
+		this.y += Math.sin(angle) * force;
+	}
 	this.update = function () {
 		//move
 		this.x += this.xvel;
 		this.y += this.yvel;
-		//repel from mouse
-		var dx = this.x - mx;
-		var dy = this.y - my;
-		var angle = Math.atan2(dy, dx);
-		var repulsion = repelFactor/Math.pow(dist(this.x, this.y, mx, my), 3);
-		if (repulsion > maxAccel) {
-			repulsion = maxAccel;
+		if (!mouseDown) {
+			//repel from mouse
+			var angle = getAngle(this.x, this.y, mx, my);
+			var force = repelFactor/Math.pow(dist(this.x, this.y, mx, my), 3);
+			if (force > maxAccel) {
+				force = maxAccel;
+			}
+			this.applyForce(angle, -force);
+		} else {
+			//attract to mouse
+			if (dist(this.x, this.y, mx, my) < pullDistance) {
+				var angle = getAngle(this.x, this.y, mx, my);
+				var force = pullFactor/Math.pow(dist(this.x, this.y, mx, my), 2);
+					if (force > maxAccel * 5) {
+						force = maxAccel*5;
+					}
+				this.applyForce(angle, force)
+			}
 		}
-		this.x += Math.cos(angle) * repulsion;
-		this.y += Math.sin(angle) * repulsion;
-		//bounce
+		//repel from each other
+		for (var i = 0; i < dots.length; i++) {
+			var dot = dots[i];
+			var distance = dist(this.x, this.y, dot.x, dot.y);
+			var	minDist = this.r + dot.r;
+			if (distance < minDist) {
+				var angle = getAngle(this.x, this.y, dot.x, dot.y);
+				var moveDistance = (minDist-distance)/2;
+				this.applyForce(angle, -1*moveDistance);
+				dot.applyForce(angle, 1*moveDistance);
+			}
+		}
+		//bounce off walls
 		if (this.x < 0) {//x
 			this.xvel *= -1;
 			this.x = Math.abs(this.x);
@@ -88,25 +132,20 @@ function dot () {
 			this.yvel *= -1;
 			this.y = ch - (this.y-ch);
 		}
-		//friction
-		/*var curSpeed = dist(0,this.xvel,0,this.yvel);
-		if (curSpeed>maxSpeed) {
-			this.xvel*=friction;
-			this.yvel*=friction;
-		}*/
 	}
 	this.draw = function () {
 		//connections
 		for (var i = 0; i < dots.length; i++) {
-			var cdot = dots[i];
-			var distance = dist(this.x, this.y, cdot.x, cdot.y);
+			var dot = dots[i];
+			var distance = dist(this.x, this.y, dot.x, dot.y);
 			if (distance <= connectionDistance+fadeOut) {
-				var transparency = (getLower(this.t, cdot.t))*2*sigmoid(-6/(2*fadeOut)*(distance-(connectionDistance+fadeOut)))-1;
+				var transparency = (getLower(this.t, dot.t))*2*sigmoid(-6/(2*fadeOut)*(distance-(connectionDistance+fadeOut)))-1;
 				//ctx.globalAlpha = (getLower(this.t, cdot.t))*2*sigmoid(-6/(2*fadeOut)*(distance-(connectionDistance+fadeOut)))-1;
 				ctx.strokeStyle = "rgba(255, 255, 255, "+transparency+")";
+				ctx.lineWidth = getLower(this.r, dot.r)/4;
 				ctx.beginPath();
 				ctx.moveTo(this.x, this.y);
-				ctx.lineTo(cdot.x, cdot.y);
+				ctx.lineTo(dot.x, dot.y);
 				ctx.stroke();
 			}
 		}
@@ -123,7 +162,9 @@ function dist (x1, y1, x2, y2) {
 }
 function sigmoid (x) {
 	return 1/(1+Math.pow(Math.E, -1*x))
-	
+}
+function getAngle (x1, y1, x2, y2) {//angle from p1 to p2
+	return Math.atan2(y2-y1, x2-x1);
 }
 function getLower (num1, num2) {
 	if (num1 < num2) {
