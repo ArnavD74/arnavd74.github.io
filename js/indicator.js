c26d31b6cc905f3863ef9592578f9a41670a23c4
Wingo206 <50254747+Wingo206@users.noreply.github.com>
2019-05-11 00:07:44 -0400

    Indiciator work

--- js/indicator.js
+++ js/indicator.js
@@ -3,6 +3,8 @@ var ccanvas = document.getElementById("scroll-canvas");
 var cctx = ccanvas.getContext("2d");
 var ccw = 0;
 var cch = 0;
+var image = document.getElementById("chevron_down");
+console.log(image);
 
 var arrows = [];
 setup();
@@ -19,18 +21,12 @@ function update () {
     cctx.clearRect(0,0,ccw,cch);
     //VV bounding box for testing
     cctx.lineWidth = 2;
-    cctx.font = "20px Arial"
     cctx.strokeStyle = "#ffffff";
     cctx.fillStyle = "#ffffff";
     cctx.beginPath();
     cctx.rect(0,0,ccw, cch);
     cctx.stroke();
-    cctx.fillText("Need", 0,80);
-    cctx.fillText("arrow", 0,100);
-    cctx.fillText("images", 0,120);
-    for (var i = 130; i < 170; i+=5) {
-        cctx.fillText("reeeeeeeeeee", i-130,i);
-    }
+    
     //
     for (var i = 0; i < arrows.length; i++) {
         arrows[i].update();
@@ -60,8 +56,9 @@ function arrow (startX, startY, width, height, shift, interval, startFrame) {
         this.y = this.startY + -1*(this.shift/2)*Math.cos((Math.PI/(this.interval/2))*(this.curFrame - this.startFrame))+(this.shift/2);
     }
     this.draw = function () {
-            cctx.beginPath();
+            /*cctx.beginPath();
             cctx.rect(this.x,this.y,this.w,this.h);
-            cctx.stroke();
+            cctx.stroke();*/
+            cctx.drawImage(image, this.x, this.y);
     }
 }
