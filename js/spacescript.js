b358d14326d0251764362bbe44d7f66cdccd8a2e
Arnav Dashaputra <arugold1@gmail.com>
2020-01-07 21:46:36 -0500

    spacelite init

--- js/spacescript.js
+++ js/spacescript.js
@@ -1,10 +1,10 @@
 function earth() {
-    window.location.href = "https://earth.google.com/web/@" + document.getElementById("copyPos").value +
+    window.location = "https://earth.google.com/web/@" + document.getElementById("copyPos").value +
         ",25.28438759a,500d,35y,0h,0t,0r";
 }
 
 function maps() {
-    window.location.href = "https://www.google.com/maps/search/?api=1&query=" + document.getElementById("copyPos").value;
+    window.location.href = 'https://www.google.com/maps/search/?api=1&query=' + document.getElementById("copyPos").value;
 }
 
 var x = document.getElementById("getPos");  
