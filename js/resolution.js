9f4db28e2ca5f6b557cc591aa603f39f6943280b
Arnav Dashaputra <arugold1@gmail.com>
2020-01-13 16:33:59 -0500

    pics

---
+++ js/resolution.js
@@ -0,0 +1,7 @@
+height = screen.height;
+width = screen.width;
+res = document.getElementById('resolutionNumber');
+res.innerHTML = "\xa0Resolution: " + width + " x " + height;
+if (res == null) {
+    alert("hello");
+}
