31fcf21b9eebb671393c1691ef6adafad379d049
Arnav Dashaputra <arugold1@gmail.com>
2020-01-27 16:32:27 -0500

    edit

--- js/resolution.js
+++
@@ -1,7 +0,0 @@
-height = screen.height;
-width = screen.width;
-res = document.getElementById('resolutionNumber');
-res.innerHTML = "\xa0Resolution: " + width + " x " + height;
-if (res == null) {
-    alert("hello");
-}
