674b2f4780bde5af743d4631d85ae3eb24e1d61e
ArnavD74 <arugold1@gmail.com>
2026-01-13 18:39:05 -0500

    add numeric nav links

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
+  resolve: { alias: { '@': '/src' } },
+  server: {
+    host: true, // == 0.0.0.0
+    port: 5173,
+    strictPort: true,
   },
-})
+});
